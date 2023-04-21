b6d5916b3a6263006faa61cb95fd8b9c72e2d5ac
Mooshaw <61675185+tsaijamey@users.noreply.github.com>
2023-04-21 11:03:46 +0800

    Update ChatGPT智能刷新.js

--- ChatGPT智能刷新.js
+++ ChatGPT智能刷新.js
@@ -5,6 +5,7 @@
 // @description  当且仅当你没有在ChatGPT页面上活动时，定时替你刷新ChatGPT的页面，间隔是30秒，如果你当前正在ChatGPT页面上，则不刷新。
 // @match        https://chat.openai.com/*
 // @grant        none
+// @author       yammi@yammi.cafe
 // ==/UserScript==
 
 
